54cbc001ea15b076ff0469f0d7581996ff1de9fc
Vova Melnyk <hagmen4339@gmail.com>
2019-04-09 21:55:07 +0300

    add Lesson 0ne

---
+++ Lesson_1 - Variables and data type/js/index.js
@@ -0,0 +1,127 @@
+'use strict'
+// JS чутливий до регістру
+// var year = 2018;
+// let month = "November";
+// const date = 27;
+// let number = 10;
+// let numBer = 50;
+// let number = 100;
+
+// let peoplename = 'Bob';
+// let peopleName = 'Bob';
+// const UAH_GRN = 8;
+
+
+
+//  ;jadf;dfjd
+/*
+skdf;sdkfsdfksdk;flskd
+lsjdflsdjflsdjflsdjflj
+dfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdf
+sdfsdfsdfsdfsdfsd
+*/
+
+let word = 'JS';
+let number = 20;
+let bool = true;
+let n = null;
+let a = undefined;
+
+let id = Symbol(5);
+
+// let name = 'Late';
+// let price = 40;
+// let currency = 'UAH';
+// let sort = 'Arabica';
+// let hasMilk = true; 
+
+// let late = {
+//     name: 'Late',
+//     price: 40,
+//     currency: 'UAH',
+//     sort: 'Arabica',
+//     hasMilk: true,
+// };
+// let lateExtra = {
+//     name: 'Late',
+//     price: 50,
+//     currency: 'UAH',
+//     sort: 'Rabusta',
+//     hasMilk: false,
+// }
+
+// typeof
+// let w = 'JS';
+// let num = 5;
+// let b = true;
+// let un = undefined;
+// let empty = null;
+// console.log(typeof w);
+// console.log(typeof num);
+// console.log(typeof b);
+// console.log(typeof un);
+// console.log(typeof empty);
+// console.log(typeof coffe);
+
+// let answer = prompt('Enter your name');
+// console.log(answer);
+// let answer2 = confirm('Are you happy?');
+// console.log(answer2);
+
+
+// Create variables
+// console.log(x);
+// var x = 5;
+// let y = 10;
+// console.log(y);
+// const z = 50;
+// console.log(z);
+
+// const
+
+// var x = 5;
+// x = 20;
+// console.log(x);
+// let y = 10;
+// y = 30;
+// console.log(y);
+// const z = 50;
+// z = 60;
+// console.log(z);
+
+
+
+// duplicate declared
+// var x = 5;
+// var x = 10;
+// console.log(x);
+
+// let y = 20;
+// let y = 30;
+// console.log(y); // Error
+
+// const z = 50;
+// const z = 60;
+// console.log(z); // Error
+
+
+// if(true) {
+//     var a = 10;
+//     console.log(a);
+// }
+
+// console.log(a);
+
+// Hoisting
+
+// console.log(x); // undefined
+// var x;
+// x = 5;
+// var x = 5;
+
+// console.log(y); // is not defined
+// let y = 10;
+// console.log('Hello');
+
+// console.log(z); // is not defined
+// const z = 50;

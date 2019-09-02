2a304555250f4fc6ec80279217cdb3b26618368c
Vova Melnyk <hagmen4339@gmail.com>
2019-09-02 22:43:33 +0300

    js folder

---
+++ JS/webpack.test.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    entry: './src/tests/appTest.js',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+        ]
+    }
+}

1c4732e0e9cb43075b0798b62ddc2e51fba60504
Volodymyr Melnyk <volodymyr.melnyk@firstbridge.io>
2019-09-10 00:40:08 +0300

    linters

--- React/app/src/App.js
+++ React/app/src/App.js
@@ -2,33 +2,29 @@ import React from 'react';
 import Sidebar from './Sidebar/Sidebar';
 import Main from './Main/Main';
 import './App.css';
-import Inline from './Inline/Inline';
-import Mod from './Mod/Mod';
 
-function App () {
-
-  let navLinks = [{
-    text: 'dashboard',
-    href: '/dashboard'
-  },
-  {
-    text: 'user profile',
-    href: '/user'
-  },
-  {
-    text: 'notifications',
-    href: '/notifications'
-  }]
+function App() {
+  const navLinks = [
+    {
+      text: 'dashboard',
+      href: '/dashboard',
+    },
+    {
+      text: 'user profile',
+      href: '/user',
+    },
+    {
+      text: 'notifications',
+      href: '/notifications',
+    },
+  ];
 
   return (
-    <div className='App-container'>
-      <Sidebar menu={navLinks}/>
-      <Main/>
-      {/* <Inline/>
-      <Mod/> */}
-   </div>
-  )
+    <div className="App-container">
+      <Sidebar menu={navLinks} />
+      <Main />
+    </div>
+  );
 }
 
 export default App;
-
